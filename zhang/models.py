22f51ec0e23b51e2633d4fad8176315a60cb8b05
Luiz <luizfernandofpl@gmail.com>
2021-07-15 09:03:01 -0300

    Zhang - model and training adjustments

--- zhang/models.py
+++ zhang/models.py
@@ -8,11 +8,13 @@ EPS = 7e-8
 class Adversarial():
     def __init__(self, initializer = GlorotNormal):
         
-        self.ini = initializer()
+        #self.ini = initializer()
+        self.ini = RandomNormal(mean=0.0, stddev=1.5)
         self.U = tf.Variable(1., name='U') #only to initialize
         #self.U = tf.Variable(self.ini(shape=(1, 3)), name='U')
         self.is_built = False
-        self.c = tf.Variable(self.ini(shape=(1,1)), name='c')
+        #self.c = tf.Variable(self.ini(shape=(1,1)), name='c')
+        self.c = tf.Variable(tf.ones([1, 1]), name='c')
 
     def __call__(self):
         pass
@@ -21,7 +23,8 @@ class Adversarial():
         return tf.Variable(self.ini(shape=shape), name='U') #check shape
 
     def get_loss(self, A, A_hat):
-        bce = BinaryCrossentropy(from_logits=True)
+        A_hat = tf.clip_by_value(A_hat, 1e-9, 1.)
+        bce = BinaryCrossentropy(from_logits=False)
         return bce(A, A_hat)
 
     '''
@@ -58,22 +61,24 @@ class Adversarial():
 
 class AdversarialDemPar(Adversarial):
     def __init__(self):
-        super(AdversarialDemPar , self).__init__()
+        super(AdversarialDemPar, self).__init__()
+        self.U = tf.Variable(self.ini(shape=(1, 1)), name='U')
+        #self.U = tf.Variable(tf.zeros([1, 1]), name='U')
 
     def __call__(self, Y, Y_hat, b):
         
-        #abs_c = 
-            
         self.S = tf.math.sigmoid(
                         tf.multiply(
                             (1+tf.math.abs(self.c)), logit(Y_hat-EPS) #here we add EPS to ensure we wont get a NaN
                         ))
 
-        if not self.is_built:
+        #self.S = Y_hat
+
+        '''if not self.is_built:
             U_shape = (1, self.S.shape[1])
             self.U = self.build(U_shape)
             #print(self.U)
-            self.is_built = True #check how to improve this build of U
+            self.is_built = True #check how to improve this build of U'''
 
         self.A_hat = tf.math.sigmoid(
                         tf.add(
@@ -85,6 +90,8 @@ class AdversarialDemPar(Adversarial):
 class AdversarialEqOdds(Adversarial):
     def __init__(self):
         super(AdversarialEqOdds, self).__init__()
+        #self.U = tf.Variable(self.ini(shape=(1, 3)), name='U')
+        self.U = tf.Variable(tf.zeros([1, 3]), name='U')
 
     def __call__(self, Y, Y_hat, b):
         
@@ -97,12 +104,13 @@ class AdversarialEqOdds(Adversarial):
                     [self.S, tf.multiply(self.S, Y), tf.multiply(self.S, 1-Y)], axis=1
                 )
         #print('conc ', concatenation.shape)
-        if not self.is_built:
+        '''if not self.is_built:
             U_shape = (1, concatenation.shape[1])
             self.U = self.build(U_shape)
             #print(self.U)
             self.is_built = True #check how to improve this build of U - getting a warning
-        #print('U ', self.U.shape)
+        #print('U ', self.U.shape)'''
+
         self.A_hat = tf.math.sigmoid(
                         tf.add(
                             tf.matmul(concatenation, tf.transpose(self.U)), 
@@ -126,7 +134,8 @@ class Classifier():
         #self.ini = initializer()
         self.ini = RandomNormal(mean=0.0, stddev=1.5)
 
-        self.W = tf.Variable(self.ini(shape=(1, xdim)), name='W')
+        #self.W = tf.Variable(self.ini(shape=(1, xdim)), name='W')
+        self.W = tf.Variable(tf.zeros([1, xdim]), name='W')
         #self.W = tf.Variable(tf.ones([1, xdim]), name='W')
 
     def __call__(self, X, b):
@@ -136,8 +145,33 @@ class Classifier():
         return self.Y_hat
 
     def get_loss(self, Y, Y_hat):
-        bce = BinaryCrossentropy(from_logits=True)
+        Y_hat = tf.clip_by_value(Y_hat, 1e-9, 1.)
+        bce = BinaryCrossentropy(from_logits=False)
         return bce(Y, Y_hat)
+
+##########################################################################################################################
+
+class UnfairLogisticRegression():
+    def __init__(self, xdim, batch_size, initializer = GlorotNormal):
+        
+        self.ini = initializer()
+        self.batch_size = batch_size
+        self.clas = Classifier(xdim)
+
+        self.b = tf.Variable(tf.ones([self.batch_size, 1]), name='b')
+        #self.b = tf.Variable(self.ini(shape=(self.batch_size, 1)), name='b')
+        #self.b = tf.Variable(tf.zeros([1,1]), name='b')
+
+    def __call__(self, X, Y, A):
+
+        #ensure casting
+        self.X = tf.dtypes.cast(X, tf.float32)
+        self.Y = tf.dtypes.cast(Y, tf.float32)
+        self.A = tf.dtypes.cast(A, tf.float32)
+
+        self.Y_hat = self.clas(self.X, self.b)
+        self.clas_loss = self.clas.get_loss(self.Y, self.Y_hat)
+        self.model_loss = self.clas_loss
 
 ##########################################################################################################################
 
@@ -151,8 +185,9 @@ class FairLogisticRegression():
         adv = self.get_adv_model(self.fairdef)
         self.adv = adv() #initializing the adversarial object
 
+        self.b = tf.Variable(tf.ones([self.batch_size, 1]), name='b')
         #self.b = tf.Variable(self.ini(shape=(self.batch_size, 1)), name='b')
-        self.b = tf.Variable(tf.zeros([1,1]), name='b')
+        #self.b = tf.Variable(tf.zeros([1,1]), name='b')
 
     def __call__(self, X, Y, A):
 
